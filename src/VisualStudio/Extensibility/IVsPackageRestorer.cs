4a31d0c4c6ca442a0b6d98a18c5d55fe0830236b
feiling <feiling@microsoft.com>
2013-11-14 16:22:13 -0800

    Start implementating VsPackageRestorer

---
+++ src/VisualStudio/Extensibility/IVsPackageRestorer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using EnvDTE;
+
+namespace NuGet.VisualStudio
+{
+    [ComImport]
+    [Guid("CAAEA598-1393-48E7-9617-1C5A62E38ABD")]
+    public interface IVsPackageRestorer
+    {
+        bool IsUserConsentGranted();
+
+        void RestorePackages(Project project);
+    }
+}
